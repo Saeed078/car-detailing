2828e981b363387dd6a4abcda47666bf8921e40b
saeed078 <sramezanpoor078@yahoo.com>
2022-08-17 12:03:51 +0430

    commit

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
-function myfunction() {
-    document.getElementById("demo").innerHTML= "this is example text" ;
+let scrollElement = document.getElementById("scroll")
+var menubtn = document.getElementById("menubtn") 
+var menu = document.getElementById("menu")
+var sidenav = document.getElementById("sidenav");
+
+window.addEventListener("scroll", function() {
+    let scrollTop = window.scrollY
+    let bodyHeight = document.body.clientHeight
+    let innerWindowHeight = window.innerHeight
+    let scrollPercent = Math.round((scrollTop / (bodyHeight - innerWindowHeight)) * 100)
+    scrollElement.style.width = scrollPercent + "%"
+    console.log(scrollPercent)
+});
+
+sidenav.style.right = "-200px"
+menubtn.onclick = function(){
+    if(sidenav.style.right == "-200px"){
+        sidenav.style.right = "0"
+        menu.src = "pic/close (1).png";
+    }
+    else{
+        sidenav.style.right = "-200px"
+        menu.src = "pic/menu (1).png";
+    }
 }
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+speed: 1000,
+speedAsDuration: true
+});
